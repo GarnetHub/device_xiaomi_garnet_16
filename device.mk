afd8f12709b4da9a74bca8ca7fa8b545d7ad5af1
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-11-02 21:49:31 +0300

    garnet: Included IntegroGapps if exists

--- device.mk
+++ device.mk
@@ -500,6 +500,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
 
+# IntegroGapps
+-include vendor/gms/gms.mk
+
 # Vendor
 $(call inherit-product, vendor/xiaomi/garnet/garnet-vendor.mk)
 
